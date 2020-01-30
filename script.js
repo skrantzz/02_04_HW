4715ad90c201e25a9a71a0310255c292a39d4986
Sydney Krantz <syd107@gmail.com>
2020-01-30 15:27:56 -0800

    first commit with pseudo code

---
+++ script.js
@@ -0,0 +1,37 @@
+// create css html read me inside folder
+// I. HTML
+// A. header
+// i. h1
+// ii. <p>
+// iii. current day
+// B. container that holds calendar (class =container)
+// i. each time is separate div
+// - separate row (class=row)
+// - columns:
+// a. time - col-md-1
+// b. input - col 10
+// c. button - col 1
+// ii. save button on right
+// iii. form for inputting text
+// iv. color code
+// -gray - past
+// -red - present
+// -green - future
+
+// II. JS
+// A. current day function (moment.js)
+// B. calendar
+// i. create save feature that captures input field & (on click) saves to my cal
+// ii. saving to local storage
+// - .setItem
+// iii. getting local storage
+// - .getItem
+
+$(".saveButton").on("click", function() {
+  event.preventDefault();
+  //   console.log(this);
+  var eventDescription = $(this);
+  console.log(eventDescription);
+  // var eventTime;
+  // localStorage.setItem(eventTime, eventDescription)
+});
